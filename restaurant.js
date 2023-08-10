46341fcd0bfe3a2f293643e9de52700af4126792
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-08-10 18:19:01 -0300

    Add files via upload

---
+++ restaurant.js
@@ -0,0 +1,34 @@
+objects = [];
+var Status = false;
+function preload(){
+    img2 = loadImage("cat.jpg");
+}
+function setup(){
+    canvas = createCanvas(img2.width,img2.height);
+    objectDetector = ml5.objectDetector("cocossd",modelLoaded)
+}
+function draw(){
+    image(img2,0,0,img2.width,img2.height);
+    fill("#ff0000");
+    noFill()
+    stroke("#ff0000");
+    if (Status == true){
+        for (i = 0; i < objects.length; i++){
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            text(objects[i].label,objects[i].x +10,objects[i].y+10);
+            document.getElementById("status").innerHTML = "detectando " + objects.length + " objetos";
+        }
+    }
+}
+function modelLoaded(){
+    console.log("model loaded!");
+    Status = true;
+    objectDetector.detect(img2,gotResults);
+}
+function gotResults(error,results){
+    if (error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
